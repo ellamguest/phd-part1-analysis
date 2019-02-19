fc3d79b06beeaa6646149bb4d99291173ff2584d
ellamguest <ellamguest@users.noreply.github.com>
2019-02-19 20:28:59 +0000

    changing to log10

--- scripts/writeup.py
+++ scripts/writeup.py
@@ -4,16 +4,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scripts.tools import addDefaults, figurePath, outputPath
 from pathlib import Path
+import seaborn as sns
 
 latexPath = lambda filename: Path(f"""latex/{filename}""")
 
 def table(df, caption=None, label=None):
+	subset = df.copy()[['log10_author_count','log10_comment_count','entropy_norm','gini','blau']]
+
 	print("\\begin{table}")
 	print("\centering")
-	print(df.describe().drop('count').to_latex())
+	print(subset.describe().drop('count').to_latex())
 	print(f"""\caption{{{caption}}}""")
 	print(f"""\label{{{label}}} """)
 	print("\end{table}")
+
+def tables(data):
+	table(data['df'], caption="Descriptive Statistics for all Subreddits", label="table:all")
+	print()
+	print()
+	table(data['defaults'], caption="Descriptive Statistics for Default Subreddits", label="table:defaults")
+	print()
+	print()
+	table(data['subset'], caption="Descriptive Statistics for Top Decile of Subreddits by Author Count", label="table:active")
 
 def subsetDecile(df, variable='author_count'):
 	"""Subsetting Active Subreddits"""
@@ -23,40 +35,52 @@ def subsetDecile(df, variable='author_count'):
 	return copy.loc[:np.round(tenth)-1]
 
 def histograms(df):
-	for v in df.select_dtypes('number').columns:
+	for v in ['log10_author_count','log10_comment_count','entropy_norm','gini','blau']:
 		print(v)
 		data = df[v]
 		filename = latexPath(f"""hist-{v}.pdf""")
-		if 'count' in v:
-			data = np.log(data)
-			filename = latexPath(f"""hist-{v}-log.pdf""")
 		plt.hist(data, color='grey')
+		plt.xlabel(v)
 		xmin, xmax = data.min(), data.max()
 		plt.xlim(xmin, xmax)
+
+		locs, labels = plt.yticks()
+		y = np.arange(0.2, 1.1, step=0.2)
+		y_scaled = y * locs.max()
+		y_labels = [x.round(1) for x in y]
+		plt.yticks(y_scaled, y_labels)
+
 		plt.tight_layout()
 		plt.savefig(filename, bbox_inches='tight')
 		plt.close()
 
-		# smaller bins
-		# kernel density over log x axis
+		# smaller bins?
 
 def kde(df):
-	for v in df.select_dtypes('number').columns:
+	for v in ['log10_author_count','log10_comment_count','entropy_norm','gini','blau']:
 		print(v)
 		data = df[v]
 		filename = latexPath(f"""kde-{v}.pdf""")
-		if 'count' in v:
-			data = np.log(data)
-			filename = latexPath(f"""kde-{v}-log.pdf""")
 		sns.kdeplot(data, shade=True, color='grey', legend=False)
+		plt.xlabel(v)
 		xmin, xmax = data.min(), data.max()
 		plt.xlim(xmin, xmax)
+
+		locs, labels = plt.yticks()
+		y = np.arange(0.2, 1.1, step=0.2)
+		y_scaled = y * locs.max()
+		y_labels = [x.round(1) for x in y]
+		plt.yticks(y_scaled, y_labels)
+
 		plt.tight_layout()
 		plt.savefig(filename, bbox_inches='tight')
 		plt.close()
 
+		# larger bins?
+		# kernel density over log x axis
+
 def settings():
-	pd.set_option('precision', 2)
+	pd.set_option('display.float_format', lambda x: '%.3f' % x)
 	plt.style.use(['seaborn-paper'])
 	plt.rc('font', monospace='Computer Modern TypeWriter')
 	plt.rc('figure', figsize=(3,3))
@@ -67,12 +91,20 @@ def plots(df):
 	histograms(df)
 	kde(df)
 
-def run():
+	# hist tiny bins OR kde larger bins
+	# hist y axis freq
+
+def loadData(date="2018-02"):
+	settings()
+
 	Path(f"""latex""").mkdir(exist_ok=True, parents=True)
 
 	date = "2018-02"
 	df = pd.read_csv(outputPath(f"""{date}/subredditLevelStats.csv"""), index_col=0)
 	df = df[~df['subreddit'].str.startswith('u_')] # dropping homepages
+
+	df['log10_author_count'] = np.log10(df['author_count'])
+	df['log10_comment_count'] = np.log10(df['comment_count'])
 
 	df['entropy_max'] = df['author_count'].apply(lambda x: np.log(x))
 	df['entropy_norm'] = df['entropy']/df['entropy_max'].fillna(0)
@@ -82,16 +114,13 @@ def run():
 
 	subset = subsetDecile(df)
 
-	table(df, caption="Descriptive Statistics for all Subreddits", label="table:all")
-	print()
-	print()
-	table(defaults, caption="Descriptive Statistics for Default Subreddits", label="table:defaults")
-	print()
-	print()
-	table(subset, caption="Descriptive Statistics for Top Decile of Subreddits by Author Count", label="table:active")
+	return {'date':date,
+			'df':df,
+			'subset':subset,
+			'defaults':defaults}
 
-	settings()
-	histograms(subset, date)
+def run():
+	data = loadData()
+	plots(data['subset'])
+	tables(data)
 
-
-	deciles(subset['blau'])
