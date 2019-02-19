e1ee89158bce010f056fa5a7f23e9efe9811bdff
ellamguest <ellamguest@users.noreply.github.com>
2019-02-19 15:24:29 +0000

    fixing latex tables

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -4,7 +4,7 @@ def trueDiversity(blau):
         blau can be expressed as a transformation of true diversity of order 2
         """
 
-        return -np.sqrt(1/(blau-1))
+        return - np.sqrt(1/(blau-1))
 
 
 
--- scripts/writeup.py
+++ scripts/writeup.py
@@ -1,52 +1,51 @@
 from scripts.dataAnalysis import *
 import numpy as np
+
 pd.set_option('precision', 2)
-
-
-date = "2018-02"
-subdf = pd.read_csv(outputPath(f"""{date}/subredditLevelStats.csv"""), index_col=0)
-
-num_subreddits = subdf.shape[0]
-
-for v in ['author_count','comment_count']:
-    subdf[v].hist(log=True)
-    plt.tight_layout()
-    plt.savefig(figurePath(f"""{date}/hist-{v}-log.pdf"""))
-
-    plt.close();
-
-# Summary of Author and Comments Counts Per Subreddit
-desc = subdf.describe().T.drop('count',axis=1)
-print(desc.to_latex())
-
-# Subsetting Active Subreddits
-corr = np.corrcoef(subdf['author_count'],subdf['comment_count'])[0][1]
-
-subdf = subdf.sort_values('author_count', ascending=False).reset_index()
-
-tenth = subdf.shape[0]/10
-subset = subdf.loc[:np.round(tenth)-1]
-
-subdf = subdf.sort_values('comment_count', ascending=False).reset_index()
-
-tenth = subdf.shape[0]/10
-comment_subset = subdf.loc[:np.round(tenth)-1]
-
-
-# Diversity Histograms
 plt.style.use(['seaborn-paper'])
 
-for v in ["entropy","blau","gini"]:
-    plt.hist(subset[v])
-    plt.tight_layout()
-    plt.savefig(figurePath(f"""{date}/hist-{v}.pdf"""))
-    plt.close();
+def sizePlots(date, df):
+        for v in ['author_count','comment_count']:
+                df[v].hist(log=True)
+                plt.tight_layout()
+                plt.savefig(figurePath(f"""{date}/hist-{v}-log.pdf"""))
+
+                plt.close();
+
+def table(df, caption=None, label=None):
+	print("\\begin{table}")
+	print("\centering")
+	print(df.describe().drop('count').to_latex())
+	print(f"""\caption{{{caption}}}""")
+	print(f"""\label{{{label}}} """)
+	print("\end{table}")
+
+def subsetDecile(df, variable='author_count'):
+        """Subsetting Active Subreddits"""
+        copy = df.copy().sort_values(variable, ascending=False).reset_index(drop=True)
+        tenth = copy.shape[0]/10
+        
+        return copy.loc[:np.round(tenth)-1]
+
+def diversityHistograms(df):
+        for v in ["entropy","blau","gini"]:
+                plt.hist(subset[v])
+                plt.tight_layout()
+                plt.savefig(figurePath(f"""{date}/hist-{v}.pdf"""))
+                plt.close();
 
 
+def run():
+	date = "2018-02"
+	df = pd.read_csv(outputPath(f"""{date}/subredditLevelStats.csv"""), index_col=0)
+	df = addDefaults(df)
+	defaults = df[df['default']==True].sort_values('author_count')
+	subset = subsetDecile(df)
 
-
-autdf = pd.read_csv(outputPath(f"""{date}/authorLevelStats.csv"""), index_col=0)
-autdf = autdf[[i for i in a.columns if 'median' in i]]
-
-
-s = subdf.describe().T
+	table(df, caption="Descriptive Statistics for all Subreddits", label="table:all")
+	print()
+	print()
+	table(defaults, caption="Descriptive Statistics for Default Subreddits", label="table:defaults")
+	print()
+	print()
+	table(subset, caption="Descriptive Statistics for Top Decile of Subreddits by Author Count", label="table:active")
